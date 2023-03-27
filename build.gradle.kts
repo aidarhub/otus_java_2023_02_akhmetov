e3413916079e4cdc34582db9d24d371ee1518187
aidarhub <aidar_050@mail.ru>
2023-03-28 00:02:44 +0300

    hw01 gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
-import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 
 plugins {
     idea
@@ -27,27 +26,11 @@ allprojects {
     }
 
     val guava: String by project
-    val testContainersBom: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         dependencies {
-            imports {
-                mavenBom(BOM_COORDINATES)
-                mavenBom("org.testcontainers:testcontainers-bom:$testContainersBom")
-            }
             dependency("com.google.guava:guava:$guava")
-        }
-    }
-}
-
-subprojects {
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        testLogging.showExceptions = true
-        reports {
-            junitXml.required.set(true)
-            html.required.set(true)
         }
     }
 }
