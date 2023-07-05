224dcf1ed6e56d797dec2a75f6e14f2092cac19f
aidarhub <aidar_050@mail.ru>
2023-07-05 22:58:28 +0300

    hw08

---
+++ hw08-io/src/main/java/org/example/dataprocessor/FileProcessException.java
@@ -0,0 +1,12 @@
+package org.example.dataprocessor;
+
+public class FileProcessException extends RuntimeException {
+
+    public FileProcessException(Exception ex) {
+        super(ex);
+    }
+
+    public FileProcessException(String msg) {
+        super(msg);
+    }
+}
